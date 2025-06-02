ec8dc45d0c1f2ae21ee41f13e19d78a11d753d7d
diegoalber1 <diegoalberone@gmail.com>
2025-06-02 20:47:38 -0300

    .

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -5,17 +5,36 @@ import numpy as np
 import joblib
 import os
 
-def download_data(symbol, start_date, end_date, save_path=None):
-    df = yf.download(symbol, start=start_date, end=end_date)
-    df = df[['Close']]
-    if save_path:
-        df.to_csv(save_path)
-    return df
+def download_data(symbol, start_date, end_date, data_path=None):
+    try:
+        df = yf.download(symbol, start=start_date, end=end_date)
+        if data_path and not (df.empty):
+            # Garante que o diretório existe
+            df = df[['Close']]
+            os.makedirs(os.path.dirname(data_path), exist_ok=True)
+            df.to_csv(data_path)
+        else:
+            if data_path and os.path.exists(data_path):
+                print("Carregando dados salvos em disco...")
+                return pd.read_csv(data_path, index_col=0)
+            else:
+                print("Arquivo de dados não encontrado em disco.")
+                return None    
+        return df
+    except Exception as e:
+        print(f"Erro ao baixar dados: {e}")
+        if data_path and os.path.exists(data_path):
+            print("Carregando dados salvos em disco...")
+            return pd.read_csv(data_path, index_col=0)
+        else:
+            print("Arquivo de dados não encontrado em disco.")
+            return None
 
 def preprocess_data(df, seq_length=60, scaler_path=None):
     scaler = MinMaxScaler()
     df['Close'] = scaler.fit_transform(df[['Close']])
     if scaler_path:
+        os.makedirs(os.path.dirname(scaler_path), exist_ok=True)
         joblib.dump(scaler, scaler_path)
     data = df['Close'].values
     X, y = [], []
